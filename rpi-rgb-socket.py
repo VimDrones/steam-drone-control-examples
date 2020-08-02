3e723dcbbf44a54d69e906e4948425193b6276a6
Huibean <huibean.luo@gmail.com>
2020-08-02 16:43:22 +0800

    init rgb socket

---
+++ rpi-rgb-socket.py
@@ -0,0 +1,23 @@
+import time
+from rpi_ws281x import *
+from neopixel import *
+
+LED_COUNT      = 6      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
+strip.begin()
+
+def show(r, g, b):
+    for i in range(LED_COUNT):
+        strip.setPixelColor(i,  Color(r, g, b))
+    strip.show()
+
+show(255,255,255)
